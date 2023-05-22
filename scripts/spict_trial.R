dd6408a731176d0abb09b9563f278865b4184fcb
arocha <alberto.jmas.rocha@gmail.com>
2023-05-22 19:44:38 +0100

    reorganizacao

---
+++ scripts/spict_trial.R
@@ -0,0 +1,77 @@
+
+# effort_ns = 
+# df %>% 
+#   filter(regiao == 'Costa Sul') %>% 
+#   filter(EGRUPART == 'MIS_MIS') %>% 
+#   group_by(year_sale, month_sale, IEMBARCA) %>% 
+#   summarise(effort = n_distinct(IDATVEND),
+#             catch = sum(QVENDA)) %>% 
+#   group_by(year_sale, month_sale) %>% 
+#   summarise(effort = sum(effort),
+#             catch = sum(catch)) %>% 
+#   ungroup() %>% 
+#   transmute(time = paste(year_sale,month_sale,sep = '_'),
+#             effort = effort,
+#             catch = catch)
+
+effort_ns = 
+  df %>% 
+  filter(regiao == 'Costa Sul') %>% 
+  filter(EGRUPART == 'MIS_MIS') %>% 
+  group_by(year_sale, IEMBARCA) %>% 
+  summarise(effort = n_distinct(IDATVEND),
+            catch = sum(QVENDA)) %>% 
+  group_by(year_sale) %>% 
+  summarise(effort = sum(effort),
+            catch = sum(catch))
+
+
+save(effort_ns, file = 'app/data/spict.Rdata')
+
+library(spict)
+
+# effort_ns$time_nm = 1995 + (1:length(effort_ns$time)/12)
+  
+  
+timeC = effort_ns$year_sale %>% as.character %>% as.numeric()
+timeI = effort_ns$year_sale %>% as.character %>% as.numeric()
+obsC = effort_ns$catch
+obsI = effort_ns$effort
+
+modelo_spict = list(timeC = timeC,
+                    timeI = timeC,
+                    obsC = obsC,
+                    obsI = obsI)
+
+modelo_spict$priors$logbkfrac <- c(log(0.8),0.5,1)
+modelo_spict$ini$logn <- log(2) #adjust production curve to Shaefer
+modelo_spict$phases$logn <- -1
+modelo_spict$priors$logalpha <- c(1, 1, 0)
+modelo_spict$priors$logbeta <- c(1, 1, 0)
+# modelo_spict$dtc = 1/12
+# modelo_spict$dteuler = 1/32
+
+res_spict = fit.spict(modelo_spict)
+retro_res = retro(res_spict)
+
+# res_spict$opt$convergence
+# res_spict$check.ini$resmat %>% print
+
+plotspict.diagnostic(calc.osa.resid(res_spict))
+
+# plot(effort_ns$effort ~ effort_ns$time_nm, type = 'l')
+# plot(effort_ns$catch ~ effort_ns$time_nm, type = 'l')
+
+plotspict.biomass(res_spict)
+plotspict.bbmsy(res_spict)
+plotspict.ffmsy(res_spict)
+plotspict.fb(res_spict)
+
+plotspict.production(res_spict)
+par(mfrow = c(2,2))
+plotspict.bbmsy(res_spict)
+plotspict.ffmsy(res_spict)
+plotspict.fb(res_spict)
+plotspict.production(res_spict, n.plotyears = 40)
+par(mfrow = c(1,1))
+plotspict.diagnostic(calc.osa.resid(res_spict))
